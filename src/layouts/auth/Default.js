18a423509396be822465e9e529ba8362b19119de
USAMA ASHRAF <usamaashraf728@gmail.com>
2025-06-04 17:40:49 +0500

    remove back button from login page

--- src/layouts/auth/Default.js
+++ src/layouts/auth/Default.js
@@ -34,7 +34,7 @@ function AuthIllustration(props) {
             width: "fit-content",
             marginTop: "40px",
           })}>
-          <Flex
+          {/* <Flex
             align='center'
             ps={{ base: "25px", lg: "0px" }}
             pt={{ lg: "0px", xl: "0px" }}
@@ -49,7 +49,7 @@ function AuthIllustration(props) {
             <Text ms='0px' fontSize='sm' color='secondaryGray.600'>
               Back to Simmmple
             </Text>
-          </Flex>
+          </Flex> */}
         </NavLink>
         {children}
         <Box
